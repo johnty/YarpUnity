262bf9d2e2ad215974e1e57b045ab8476bdc344c
johnty <johntywang@gmail.com>
2014-03-11 17:38:26 -0700

    adding yarp unity project with swig generated c# files

---
+++ Assets/myScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myScript : MonoBehaviour {
+
+	BufferedPortBottle p;
+	// Use this for initialization
+	void Start () {
+		Network.init();
+		p = new BufferedPortBottle();
+		p.open ("/unityYarp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			gameObject.renderer.material.color = Color.red;
+				}
+		if (Input.GetKeyDown (KeyCode.G)) {
+			gameObject.renderer.material.color = Color.green;
+		}
+		if (Input.GetKeyDown (KeyCode.B)) {
+			gameObject.renderer.material.color = Color.blue;
+		}
+
+		if (p.getPendingReads () > 0) {
+			gameObject.renderer.material.color = Color.cyan;
+			Bottle bot = p.read();
+			if (bot.size() == 3) {
+				int r = bot.get (0).asInt();
+				int g = bot.get (1).asInt();
+				int b = bot.get (2).asInt();
+				gameObject.renderer.material.color= new Color(r, g, b);
+			}
+			if (bot.size () == 1) {
+				transform.Translate(0, 5, 0);
+			}
+		}
+
+	}
+}
